05399feff7e885e4aea2425b6cf6f25e1e8152e8
erekJiang <erekjiang@gmail.com>
2020-03-21 09:45:13 +0800

    revert error

--- src/stack/MyStringStack.java
+++ src/stack/MyStringStack.java
@@ -33,9 +33,8 @@ public class MyStringStack {
      * @see         String
      */
     public String pop() throws NoSuchElementException {
-        stack.push (s1);
-
-        //throw new NoSuchElementException();
+        
+    	//throw new NoSuchElementException();
     	String result = v.lastElement();
     	v.remove(result);
     	System.out.println("hello");
